8bc3fabed7b8093afbcf55b0dffb63057675a963
Netanel Amar <NetanelCA2@gmail.com>
2025-04-01 14:05:59 +0300

    Code clean

--- app/src/main/java/com/netanel/ibihometest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/netanel/ibihometest/ui/home/HomeFragment.kt
@@ -9,12 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.netanel.ibihometest.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class
+HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
